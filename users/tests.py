f0a5ea922594f52c5ff4026ed2a7853d7bbc2b91
i-Krypt <vamay505@gmail.com>
2019-10-30 12:56:52 +0300

    restructured test files

--- users/tests.py
+++ users/tests.py
@@ -3,27 +3,27 @@ from .models import Projects,Profile
 
 
 # Create your tests here.Projects
-class Projects(TestCase):
+# class Projects(TestCase):
 
-    #set up method
-    def setUp(self):
-        self.description= Projects(location='description')
+#     #set up method
+#     def setUp(self):
+#         self.description= Projects(location='description')
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.description,Projects))
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.description,Projects))
 
-    def tearDown(self):
-        Projects.objects.all().delete()
+#     def tearDown(self):
+#         Projects.objects.all().delete()
 
-    def test_save_method(self):
-        self.description.save_location()
-        projects = Projects.objects.all()
-        self.assertTrue(len(projects)>0)
+#     def test_save_method(self):
+#         self.description.save_location()
+#         projects = Projects.objects.all()
+#         self.assertTrue(len(projects)>0)
 
-    def test_delete_method(self):
-        self.description.delete_location('description')
-        projects = Projects.objects.all()
-        self.assertTrue(len(projects)==0)
+#     def test_delete_method(self):
+#         self.description.delete_location('description')
+#         projects = Projects.objects.all()
+#         self.assertTrue(len(projects)==0)
 
 # class ProfileTestClass(TestCase):
 #    def setUp(self):
