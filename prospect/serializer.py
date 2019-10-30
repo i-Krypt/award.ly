b1a12b53e8a0a414c0698bff787709cc9cf05d27
i-Krypt <vamay505@gmail.com>
2019-10-30 13:29:23 +0300

    file changes to app

--- prospect/serializer.py
+++ prospect/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Projects,Profile
+# from .models import Projects,Profile
 
 class ProjectsSerializer(serializers.ModelSerializer):
     class Meta:
